9bdf0762980aa9528f9be12ebf3a516a35ed45b7
sofahmm <74815523+sofahmm@users.noreply.github.com>
2022-10-19 14:19:58 +0300

    first commit

---
+++ ConcertnicApp/Pages/AuthorizatonPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConcertnicApp.Pages
+{
+    /// <summary>
+    /// Interaction logic for AuthorizatonPage.xaml
+    /// </summary>
+    public partial class AuthorizatonPage : Page
+    {
+        public AuthorizatonPage()
+        {
+            InitializeComponent();
+        }
+
+        private void registrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistrationPage());
+        }
+
+        private void authorizationBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
